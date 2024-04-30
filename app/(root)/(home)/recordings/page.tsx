1c7f1c5a9cf9b651739318131b5eec5baa57e0fe
LarinNikita <larin_nikita_m@mail.ru>
2024-04-30 16:44:33 +0700

    sidebar and pages

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const HomePage = () => {
+function RecordingsPage() {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <h1 className="text-3xl font-bold">RecordingsPage</h1>
     </section>
   );
-};
+}
 
-export default HomePage;
+export default RecordingsPage;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,50 @@
+'use client';
+
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+
+import { sidebarLinks } from '@/constants';
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const pathname = usePathname();
+
+  return (
+    <section className="bg-dark-1 sticky left-0 top-0 flex h-screen w-fit flex-col justify-between p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map(item => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn(
+                'flex items-center justify-start gap-4 rounded-lg p-4',
+                {
+                  'bg-blue-1': isActive,
+                },
+              )}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Sidebar;
