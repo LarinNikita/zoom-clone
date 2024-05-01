05fe5f2feaf9776e525a8bbf6112691c70dd6bed
LarinNikita <larin_nikita_m@mail.ru>
2024-05-01 12:54:23 +0700

    meetings calls and custom hook

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+'use client';
 
-type Props = {
+import React, { useState } from 'react';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+
+import { useGetCallById } from '@/hooks/useGetCallById';
+
+import Loader from '@/components/Loader';
+import MeetingSetup from '@/components/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom';
+
+type MeetingPageIdProps = {
   params: {
     id: string;
   };
 };
 
-const MeetingPageId = ({ params }: Props) => {
-  return <div>Meeting Room #{params.id}</div>;
+const MeetingPageId = ({ params }: MeetingPageIdProps) => {
+  const { user, isLoaded } = useUser();
+  const [isSetupCompleted, setIsSetupCompleted] = useState(false);
+  const { call, isCallLoading } = useGetCallById(params.id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupCompleted ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default MeetingPageId;
