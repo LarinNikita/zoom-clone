55c95eef79817d09bc4d571ca191d72f1ae064b8
LarinNikita <larin_nikita_m@mail.ru>
2024-05-01 11:20:47 +0700

    home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import MeetingTypeList from '@/components/MeetingTypeList';
 
 const HomePage = () => {
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date());
+    }, 10000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  const date = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'full',
+  }).format(now);
+
   return (
-    <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+    <section className="mx-auto flex size-full max-w-screen-xl flex-col gap-10 text-white">
+      <div className="bg-hero h-[300px] w-full rounded-[20px] bg-cover">
+        <div className="flex h-full flex-col justify-between p-8 max-md:px-5 max-md:py-8 lg:p-11">
+          {/* TODO: make dynamic time */}
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 15:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-sky-1 text-lg font-medium lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
+      <MeetingTypeList />
     </section>
   );
 };
